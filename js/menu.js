bca53d16b0d3f8817b93a2430195ce33b462752c
Dan Gold <dan97gold@hotmail.com>
2019-08-12 12:50:56 +0100

    Refactor Navbar and SCSS

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,26 @@ menuButton.addEventListener('click', function toggleMenu(){
     menuButton.classList.toggle('toggle--active');
     body.classList.toggle('mobile__menu--active');
 });
+
+const navItems = document.querySelectorAll('.navigation__links > li');
+
+// On click
+
+navItems.forEach ( element => {
+  element.addEventListener('mouseout', function(){
+    element.classList.remove('submenu--active');
+  });
+});
+
+
+window.addEventListener('click', function(e){
+
+  navItems.forEach(element => {
+    if (element.contains(e.target)){
+      element.classList.toggle('submenu--active');
+    }
+    else {
+      element.classList.remove('submenu--active');
+    }
+  });
+});
